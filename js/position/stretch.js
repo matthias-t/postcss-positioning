b2693d617aa6d3f1844956f7aca5fe916d0adde1
Matthias Totschnig <matthias@totschnig.org>
2018-02-10 18:06:40 +0100

    Implement `stretchValue`

--- js/position/stretch.js
+++ js/position/stretch.js
@@ -3,3 +3,11 @@ export const isStretch = (length) => {
     const unit = length.match(/[A-z]+/);
     return unit !== null && unit[0] === 's';
 };
+
+// Get the number before the 's' unit
+export const stretchValue = (length) => {
+    if (!isStretch(length)) {
+        throw new Error(length + ' is not a stretch length');
+    }
+    return length.slice(0, -1);
+};
