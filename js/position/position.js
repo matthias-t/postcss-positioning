789e84b022859029a0cc92fcdace376ffe88cfd5
Matthias Totschnig <matthias@totschnig.org>
2018-02-18 20:31:13 +0100

    Add two utility methods to Position class

--- js/position/position.js
+++ js/position/position.js
@@ -1,5 +1,7 @@
 // The position data type that stores position info
 
+import { direction } from '../enum';
+
 export default class Position {
 
     constructor({ horizontal, vertical, align, type }) {
@@ -7,5 +9,23 @@ export default class Position {
         this.vertical = vertical;
         if (align) this.align = align;
         if (type) this.type = type;
+    }
+
+    iterateDirections(callback) {
+        [
+            {
+                dir: direction.horizontal,
+                lengths: Object.values(this.horizontal)
+            }, {
+                dir: direction.vertical,
+                lengths: Object.values(this.vertical)
+            }
+        ].forEach(({ dir, lengths }) => {
+            callback(dir, lengths);
+        });
+    }
+
+    isAlignedInDirection(dir) {
+        return this.align && this.align.direction === dir;
     }
 }
