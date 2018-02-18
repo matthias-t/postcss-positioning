7103e9fe65d4efcee34a213a26417c38bed3db54
Matthias Totschnig <matthias@totschnig.org>
2018-02-18 15:35:33 +0100

    Ignore opts being unused for now

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import postcss from 'postcss';
 import process from './js/process.js';
 
+/* eslint no-unused-vars: "off" */
+
 const isPositionDecl = node => {
     return node.type === 'decl' &&
         ['horizontal', 'vertical', 'type'].some(prop => prop === node.prop);
