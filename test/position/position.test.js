486f4e23cba965d862ec82e5ff038fa28b5f30aa
Matthias Totschnig <matthias@totschnig.org>
2018-05-15 21:26:01 +0200

    Add more tests for `Position` class

--- test/position/position.test.js
+++ test/position/position.test.js
@@ -1,7 +1,22 @@
 import Position from '../../js/position/position';
 import { direction } from '../../src/enum';
 
-const position = new Position({
+/* eslint max-len: "off" */
+
+const positionA = new Position({
+    horizontal: {
+        before: '1s',
+        size: '7.3s',
+        after: '2px'
+    },
+    vertical: {
+        before: 'calc(5px * 7 / 23)',
+        size: '1px',
+        after: '0'
+    }
+});
+
+const positionB = new Position({
     horizontal: {
         before: '1px',
         after: '2px'
@@ -13,15 +28,45 @@ const position = new Position({
     }
 });
 
+describe('iterateDirections', () => {
+    it('iterates directions', () => {
+        const mockA = jest.fn();
+        positionA.iterateDirections(mockA);
+        expect(mockA).toHaveBeenCalledTimes(2);
+        expect(mockA).toHaveBeenCalledWith(
+            direction.horizontal, ['1s', '7.3s', '2px']);
+        expect(mockA).toHaveBeenLastCalledWith(
+            direction.vertical, ['calc(5px * 7 / 23)', '1px', '0']);
+
+        const mockB = jest.fn();
+        positionB.iterateDirections(mockB);
+        expect(mockB).toHaveBeenCalledTimes(2);
+        expect(mockB).toHaveBeenCalledWith(
+            direction.horizontal, ['1px', '2px']);
+        expect(mockB).toHaveBeenLastCalledWith(
+            direction.vertical, ['3px', '1px', '4px']);
+    });
+});
+
 describe('iterateLengths', () => {
     it('iterates lengths', () => {
-        const mock = jest.fn();
-        position.iterateLengths(mock);
-        expect(mock).toHaveBeenCalledTimes(5);
-        expect(mock).toHaveBeenCalledWith(direction.horizontal, 'before', '1px');
-        expect(mock).toHaveBeenCalledWith(direction.horizontal, 'after', '2px');
-        expect(mock).toHaveBeenCalledWith(direction.vertical, 'before', '3px');
-        expect(mock).toHaveBeenCalledWith(direction.vertical, 'size', '1px');
-        expect(mock).toHaveBeenLastCalledWith(direction.vertical, 'after', '4px');
+        const mockA = jest.fn();
+        positionA.iterateLengths(mockA);
+        expect(mockA).toHaveBeenCalledTimes(6);
+        expect(mockA).toHaveBeenCalledWith(direction.horizontal, 'before', '1s');
+        expect(mockA).toHaveBeenCalledWith(direction.horizontal, 'size', '7.3s');
+        expect(mockA).toHaveBeenCalledWith(direction.horizontal, 'after', '2px');
+        expect(mockA).toHaveBeenCalledWith(direction.vertical, 'before', 'calc(5px * 7 / 23)');
+        expect(mockA).toHaveBeenCalledWith(direction.vertical, 'size', '1px');
+        expect(mockA).toHaveBeenLastCalledWith(direction.vertical, 'after', '0');
+
+        const mockB = jest.fn();
+        positionB.iterateLengths(mockB);
+        expect(mockB).toHaveBeenCalledTimes(5);
+        expect(mockB).toHaveBeenCalledWith(direction.horizontal, 'before', '1px');
+        expect(mockB).toHaveBeenCalledWith(direction.horizontal, 'after', '2px');
+        expect(mockB).toHaveBeenCalledWith(direction.vertical, 'before', '3px');
+        expect(mockB).toHaveBeenCalledWith(direction.vertical, 'size', '1px');
+        expect(mockB).toHaveBeenLastCalledWith(direction.vertical, 'after', '4px');
     });
 });
