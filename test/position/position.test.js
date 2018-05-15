ffd55472e7d4160738f2d5b87bd0e4b8dbb94583
Matthias Totschnig <matthias@totschnig.org>
2018-05-15 17:50:45 +0200

    Use Jest mock functions to test `iterateLengths`
    Position.prototype.iterateLengths() in src/position/position.js

--- test/position/position.test.js
+++ test/position/position.test.js
@@ -1,29 +1,27 @@
 import Position from '../../js/position/position';
 import { direction } from '../../src/enum';
 
+const position = new Position({
+    horizontal: {
+        before: '1px',
+        after: '2px'
+    },
+    vertical: {
+        before: '3px',
+        size: '1px',
+        after: '4px'
+    }
+});
+
 describe('iterateLengths', () => {
     it('iterates lengths', () => {
-        const position = new Position({
-            horizontal: {
-                before: '1px',
-                after: '2px'
-            },
-            vertical: {
-                before: '3px',
-                size: '1px',
-                after: '4px'
-            }
-        });
-        const all = [];
-        position.iterateLengths( (dir, length, value) => {
-            all.push([dir, length, value]);
-        });
-        expect(all).toEqual([
-            [direction.horizontal, 'before', '1px'],
-            [direction.horizontal, 'after', '2px'],
-            [direction.vertical, 'before', '3px'],
-            [direction.vertical, 'size', '1px'],
-            [direction.vertical, 'after', '4px']
-        ]);
+        const mock = jest.fn();
+        position.iterateLengths(mock);
+        expect(mock).toHaveBeenCalledTimes(5);
+        expect(mock).toHaveBeenCalledWith(direction.horizontal, 'before', '1px');
+        expect(mock).toHaveBeenCalledWith(direction.horizontal, 'after', '2px');
+        expect(mock).toHaveBeenCalledWith(direction.vertical, 'before', '3px');
+        expect(mock).toHaveBeenCalledWith(direction.vertical, 'size', '1px');
+        expect(mock).toHaveBeenLastCalledWith(direction.vertical, 'after', '4px');
     });
 });
